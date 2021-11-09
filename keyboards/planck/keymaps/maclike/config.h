6dfee21ed0504c84b3ac85c2292972dbbc7c6b20
rob <rob@angle.fish>
2021-11-09 14:51:58 -0800

    update planck with new maclike code

--- keyboards/planck/keymaps/maclike/config.h
+++ keyboards/planck/keymaps/maclike/config.h
@@ -7,3 +7,14 @@
 #define AUDIO_CLICKY
 
 #define DAC_SAMPLE_MAX 30000U//65535U
+
+#ifdef AUDIO_ENABLE
+
+// Mac chord 
+#define MAC_SOUND E__NOTE(_FS4), E__NOTE(_AS4), Q__NOTE(_CS5), W__NOTE(_FS5)
+
+// XP Startup
+#define WIN_SOUND W__NOTE(_DS5), S__NOTE(_DS4), H__NOTE(_AS4), H__NOTE(_GS4), H__NOTE(_DS4), H__NOTE(_DS5), H__NOTE(_AS4)
+
+#define DEFAULT_LAYER_SONGS { SONG(MAC_SOUND), SONG(WIN_SOUND)}
+#endif
