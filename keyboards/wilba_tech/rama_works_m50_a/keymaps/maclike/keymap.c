49af1e8999852faebdaefc5212b5f74b432250ee
anglefishrob <rob@angle.fish>
2022-09-19 13:00:47 -0700

    updated space layout

--- keyboards/wilba_tech/rama_works_m50_a/keymaps/maclike/keymap.c
+++ keyboards/wilba_tech/rama_works_m50_a/keymaps/maclike/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_2,    ESC_RSE, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    COL_NAV, ENT_FN,
     KC_3,    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT,
-    KC_4,    KC_LCTL, KC_LALT, KC_LCMD, XXXXXXX, SPC_RSE,    KC_SPC,        SPC_RSE, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_4,    KC_LCTL, KC_LALT, KC_LCMD, XXXXXXX, KC_SPC,     KC_SPC,        SPC_RSE, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 [WIN_LAYER] = LAYOUT(
