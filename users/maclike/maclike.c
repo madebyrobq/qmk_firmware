31f77ff98c2b13a7e357693e6ef525fed086dc1f
madebyrobq <45523916+madebyrobq@users.noreply.github.com>
2024-11-26 15:54:42 -0800

    clean up overrides

--- users/maclike/maclike.c
+++ users/maclike/maclike.c
@@ -68,7 +68,7 @@ bool process_maclike_win_mode_key(uint16_t keycode, bool pressed){
     return true;
 
   bool lctrl_pressed = get_mods() & MOD_BIT(KC_LCTL);
-  bool alt_pressed = get_mods() & MOD_BIT(KC_LALT);
+  //bool alt_pressed = get_mods() & MOD_BIT(KC_LALT);
   bool cmd_pressed = get_mods() & MOD_BIT(WIN_CMD);
 
   // Cmd + Tab = open task switcher, and continue
@@ -87,44 +87,12 @@ bool process_maclike_win_mode_key(uint16_t keycode, bool pressed){
   // Suppress annoying menu bar focus immediately after registering Alt
   if(keycode == KC_LALT && pressed){
     register_code(keycode);
-    unfocus_menu_bar();
-  }
-
-  // Alt + L/R = Ctrl + L/R (move cursor one word)
-  if(keycode == KC_LEFT || keycode == KC_RIGHT){
-    if(pressed && alt_pressed && !cmd_pressed){
-        unregister_code(KC_LALT);
-        register_code(KC_RCTL);
-        register_code(keycode);
-        unregister_code(KC_RCTL);
-        register_code(KC_LALT);
-        unfocus_menu_bar(); // unfocus menu bar after registering Alt
-        return false;
-    }
-  }
-
-  // Alt + Backspace = Ctrl + Backspace (delete one word)
-  if(keycode == KC_BSPC && pressed && alt_pressed && !cmd_pressed){
     unregister_code(KC_LALT);
-    register_code(KC_RCTL);
-    register_code(keycode);
-    unregister_code(KC_RCTL);
     register_code(KC_LALT);
-    unfocus_menu_bar(); // unfocus menu bar after registering Alt
-    return false;
-  }
-
-  // Ctrl + Cmd + Space = Emojis
-  if(keycode == KC_SPC && pressed && lctrl_pressed && cmd_pressed){
-    unregister_code(WIN_CMD);
-    unregister_code(KC_LCTL);
-    SEND_STRING(SS_LGUI("."));
-    register_code(WIN_CMD);
-    register_code(KC_LCTL);
-    return false;
   }
 
   // Cmd + Space = Windows key, which can be tapped for search or held for Windows commands
+  // Key Overrides don't support hold
   if(keycode == KC_SPC){
     if(pressed && cmd_pressed && !lctrl_pressed){
       unregister_code(WIN_CMD);
@@ -168,18 +136,18 @@ __attribute__ ((weak)) bool process_record_user(uint16_t keycode, keyrecord_t *r
   return true;
 }
 
-// Basic Shift overrides for different symbols
+// Basic Shift overrides for different symbols, for Shift only
 const key_override_t comma_exclamation_override = ko_make_with_layers_negmods_and_options(
   MOD_MASK_SHIFT,
   KC_COMMA,
-  KC_EXCLAIM,
+  KC_EXLM,
   ~0,
   MOD_MASK_CAG,
   ko_option_activation_trigger_down);
 const key_override_t period_question_override = ko_make_with_layers_negmods_and_options(
   MOD_MASK_SHIFT,
   KC_DOT,
-  KC_QUESTION,
+  KC_QUES,
   ~0,
   MOD_MASK_CAG,
   ko_option_activation_trigger_down);
@@ -195,7 +163,7 @@ const key_override_t slash_backslash_override = ko_make_with_layers_negmods_and_
 // "Cmd" + L/R = home/end
 bool win_mode_key_override(bool pressed, void *context) { return win_mode(); }
 const key_override_t cmd_right_override = {
-  .trigger_mods          = MOD_BIT(WIN_CMD),
+  .trigger_mods           = MOD_BIT(WIN_CMD),
   .layers                 = ~0,
   .suppressed_mods        = MOD_BIT(WIN_CMD),
   .options                = ko_option_activation_trigger_down,
@@ -219,7 +187,7 @@ const key_override_t cmd_left_override = {
 
 // Alt + L/R = Ctrl + L/R
 // NOTE: this doesn't work well with key overrides as it focuses the menu bar every other time...
-/*const key_override_t alt_right_override = {
+const key_override_t alt_right_override = {
   .trigger_mods          = MOD_BIT(KC_LALT),
   .layers                 = ~0,
   .suppressed_mods        = MOD_BIT(KC_LALT),
@@ -240,7 +208,7 @@ const key_override_t alt_left_override = {
   .context                = NULL,
   .trigger                = KC_LEFT,
   .replacement            = RCTL(KC_LEFT),
-  .enabled                = NULL};*/
+  .enabled                = NULL};
 
 // "Cmd" + Backspace = Delete
 const key_override_t cmd_backspace_override = {
@@ -255,22 +223,9 @@ const key_override_t cmd_backspace_override = {
   .replacement            = KC_DEL,
   .enabled                = NULL};
 
-// "Cmd" + Space = Windows key (tap for search)
-/*const key_override_t cmd_space_override = {
-  .trigger_mods           = MOD_BIT(WIN_CMD),
-  .layers                 = ~0,
-  .suppressed_mods        = MOD_BIT(WIN_CMD),
-  .options                = ko_option_activation_trigger_down,
-  .negative_mod_mask      = (uint8_t) ~MOD_BIT(WIN_CMD),
-  .custom_action          = win_mode_key_override,
-  .context                = NULL,
-  .trigger                = KC_SPC,
-  .replacement            = KC_LGUI,
-  .enabled                = NULL};*/
-
 // "Cmd" + LCTRL + Space = emojis
 // NOTE: Key overrides with Left and Right sides active for EITHER not BOTH
-/*const key_override_t cmd_ctrl_space_override = {
+const key_override_t cmd_ctrl_space_override = {
   .trigger_mods           = MOD_BIT(WIN_CMD) | MOD_BIT(KC_LCTL),
   .layers                 = ~0,
   .suppressed_mods        = MOD_BIT(WIN_CMD) | MOD_BIT(KC_LCTL),
@@ -280,21 +235,7 @@ const key_override_t cmd_backspace_override = {
   .context                = NULL,
   .trigger                = KC_SPC,
   .replacement            = LGUI(KC_DOT),
-  .enabled                = NULL};*/
-
-// "Cmd" + Tab = Alt + Tab and hold Alt
-// NOTE: this doesn't hold the task switcher open which is annoying
-/*const key_override_t cmd_tab_override = {
-  .trigger_mods           = MOD_BIT(WIN_CMD),
-  .layers                 = ~0,
-  .suppressed_mods        = MOD_BIT(WIN_CMD),
-  .options                = ko_option_activation_trigger_down,
-  .negative_mod_mask      = (uint8_t) 0,
-  .custom_action          = win_mode_key_override,
-  .context                = NULL,
-  .trigger                = KC_TAB,
-  .replacement            = LALT(KC_TAB),
-  .enabled                = NULL};*/
+  .enabled                = NULL};
 
 const key_override_t cmd_option_esc_override = {
   .trigger_mods           = MOD_BIT(WIN_CMD) | MOD_BIT(KC_LALT),
@@ -304,22 +245,23 @@ const key_override_t cmd_option_esc_override = {
   .negative_mod_mask      = (uint8_t) 0,
   .custom_action          = win_mode_key_override,
   .context                = NULL,
-  .trigger                = ESC_RSE,
+  .trigger                = KC_ESC,
   .replacement            = LCA(KC_DEL),
   .enabled                = NULL};
 
 const key_override_t **key_overrides = (const key_override_t *[]){
+    // Shift overrides
     &comma_exclamation_override,
     &period_question_override,
     &slash_backslash_override,
+
+    // Cmd overrides
+    &cmd_backspace_override,
+
+    // Windows-to-Mac overrides
     &cmd_right_override,
     &cmd_left_override,
-    //&alt_right_override,
-    //&alt_left_override,
-    &cmd_backspace_override,
-    //&cmd_space_override,
-    //&cmd_ctrl_space_override,
-    //&cmd_tab_override,
+    &cmd_ctrl_space_override,
     &cmd_option_esc_override,
 
     NULL // null terminator
